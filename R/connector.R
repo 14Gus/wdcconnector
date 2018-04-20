1c81df98bc8e51f9ea062f132fe9fcaebf5230d5
Angus Moir <am@altimapa.com>
2018-04-20 16:11:35 +0100

    Revert "Tests passing except table name"
    This reverts commit 2cacd0e2856c4344ab94eaf1b6ef463932005906.

--- R/connector.R
+++
@@ -1,9 +0,0 @@
-generateWDCConnectorJS <- function(table){
-
-  table_name <- substitute(table)
-
-  template <- getTemplate("WDC_connector_template.js")
-
-  whisker::whisker.render(template)
-
-}
--- templates/WDC_connector_template.R
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  $("#submitButton").click(function () {
-    tableau.connectionName ="{{table_name}} Table Feed";
-    tableau.submit();
-  });
-});
